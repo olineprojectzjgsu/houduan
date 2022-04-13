73b39120d70f15658f21dbb5b1f1110401513538
root <root@localhost.localdomain>
2022-04-13 07:55:38 -0400

    目前提交所有后端代码

---
+++ takeout_01/user/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    # CharField类型不能为空,最少要指定一个长度
+    #DecimialField表示浮点型数，max_digits表示小数点前最大位数，decimal_places表示小数点后最多位数，并且必须指派一个默认值
+    user_name = models.CharField(max_length=32,default='')
+    email = models.EmailField(max_length=32)
+    pwd = models.CharField(max_length=32,default='')
+    phone = models.IntegerField(default=0)
+    nickname = models.CharField(max_length=20,default='')
+
+    class Meta:
+        db_table = 'userinfo'
+
+    def __str__(self):
+        return 'user_name is %s email is %s phone is %f'%(self.user_name,self.email,self.phone)
