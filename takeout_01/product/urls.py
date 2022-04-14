2211cb78ec8f50b5211423ad6255f3e63b9caa70
root <root@localhost.localdomain>
2022-04-14 06:15:16 -0400

    这次提交的大多数都是接口调试的代码， 然后把接口文档完善了以下

---
+++ takeout_01/product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('all_product',views.all_product),
+    path('update_product/<int:product_id>',views.update_product),
+    path('delete_product',views.delete_product),
+]
+# p1 = Product(product_name="荔枝",price=32.23,describation="超级甜",saler_id=1,sort="水果")
