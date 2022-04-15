695014d61c73f4df2df0ba9ea3b3a122ddee3127
root <root@localhost.localdomain>
2022-04-15 03:40:36 -0400

    成功通过POST添加数据

--- takeout_01/product/views.py
+++ takeout_01/product/views.py
@@ -1,33 +1,27 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect, JsonResponse
 from product.models import Product
 
 # Create your views here.
 
-def all_product(request):
+def all_product(request):#查询产品，采用GET请求
     if request.method == 'GET':
         all_product= Product.objects.values()#产品的所有信息
         product_json = list(all_product)#输出产品信息的json格式
-        return JsonResponse({'ret':0, 'retlist': product_json})
-def update_product(request,product_id):
-    try:
-        product =  Product.objects.get(id=product_id,is_active=True);
-    
-    except Exception as e:
-        print('--update product error is %s'%(e))
-        return HttpResponse('--The product is not existed')
-    if request.method == 'GET':
-        pass
-        #暂时先这样
-    elif request.method == 'POST':
-        price = request.POST['price']
-        print(price)
-        market_price = request.POST['price']
-        print(market_price)
-        product.price = price
-        product.market_price = market_price
-        product.save()
-        return HttpResponse('change successfully') 
+        return JsonResponse({'ret':0, 'data': product_json})
+def update_product(request):
+    if request.method == 'POST':
+        request.params = json.loads(request.body)
+        info = request.params['data']
+        new_product = Product.objects.create(product_name=info['product_name'],
+            price = info['price'],
+            market_price = info['market_price'],
+            saler_id = info['saler_id'],
+            sort = info['sort'],
+            describation = info['describation'],
+        )
+    return JsonResponse({'ret':0,'id':new_product.id})
 def delete_product(request):
     product_id = request.GET.get('product_id')
     if not product_id:
