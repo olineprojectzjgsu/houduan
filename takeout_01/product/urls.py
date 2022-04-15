6fae7dd0b1483c1f90f4f939a6e57eed027aa77d
root <root@localhost.localdomain>
2022-04-15 06:45:26 -0400

    这次完成了PUT操作，即修改数据，以及GET获取单一数据的方法，还有删除数据的操作

--- takeout_01/product/urls.py
+++ takeout_01/product/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('all_product',views.all_product),
     path('update_product',views.update_product),
     path('delete_product',views.delete_product),
+    path('add_product',views.add_product),
+    path('get_sure_product',views.get_sure_product),
 ]
 # p1 = Product(product_name="荔枝",price=32.23,describation="超级甜",saler_id=1,sort="水果")
