6a7894ef6cdd3b2c02f202d8e1048db1361597c9
root <root@localhost.localdomain>
2022-04-15 06:45:26 -0400

    这次完成了PUT操作，即修改数据，以及GET获取单一数据的方法，还有删除数据的操作

--- takeout_01/product/views.py
+++ takeout_01/product/views.py
@@ -10,7 +10,20 @@ def all_product(request):#查询产品，采用GET请求
         all_product= Product.objects.values()#产品的所有信息
         product_json = list(all_product)#输出产品信息的json格式
         return JsonResponse({'ret':0, 'data': product_json})
-def update_product(request):
+def get_sure_product(request):#查询产品，采用GET请求
+    if request.method == 'GET':
+        request.params = json.loads(request.body)
+        info = request.params['data']
+        try:
+            select_product= Product.objects.filter(id=info['id']).values()#产品的所有信息
+        except Product.DoesNotExist:
+            return{
+                'ret':1,
+                'msg': "数据不存在"
+            }
+        product_json = list(select_product)#输出产品信息的json格式
+        return JsonResponse({'ret':0, 'data': product_json})
+def add_product(request):
     if request.method == 'POST':
         request.params = json.loads(request.body)
         info = request.params['data']
@@ -23,14 +36,43 @@ def update_product(request):
         )
     return JsonResponse({'ret':0,'id':new_product.id})
 def delete_product(request):
-    product_id = request.GET.get('product_id')
-    if not product_id:
-        return HttpResponse("请求异常")
-    try:
-        product= Product.objects.get(id=product_id,is_active=True)
-    except Exception as e:
-        print('----delete book get error is %s'%(e))
-        return HttpResponse('---The book id is error')
-    product.is_active=False
-    product.save()
-    pass
+    if request.method == 'DELETE':
+        request.params = json.loads(request.body)
+        info = request.params['data']
+        try:
+            select_product = Product.objects.get(id=info['id'])
+        except Product.DoesNotExist:
+            return{
+                'ret':1,
+                'msg': "数据不存在"
+            }
+        select_product.is_active = False
+        select_product.save()
+        return JsonResponse({'ret':0})
+def update_product(request):
+    if request.method == 'PUT':
+        request.params = json.loads(request.body)
+        newdata = request.params['newdata']
+        try:
+            select_product = Product.objects.get(id=newdata['id'])
+        except Product.DoesNotExist:
+            return{
+                'ret':1,
+                'msg': "数据不存在"
+            }
+        if 'product_name' in newdata:
+            select_product.product_name = newdata['product_name']
+        if 'price' in newdata:
+            select_product.price = newdata['price'] 
+        if 'market_price' in newdata:
+            select_product.market_price = newdata['market_price'] 
+        if 'is_active' in newdata:
+            select_product.is_active = newdata['is_active'] 
+        if 'saler_id' in newdata:
+            select_product.saler_id = newdata['saler_id'] 
+        if 'sort' in newdata:
+            select_product.sort = newdata['sort'] 
+        if 'describation' in newdata:
+            select_product.describation = newdata['describation'] 
+        select_product.save()
+        return JsonResponse({'ret':0})
