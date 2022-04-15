6b69b464c789b97ee2cb9862452f55309933e3c8
root <root@localhost.localdomain>
2022-04-15 08:27:36 -0400

    目前已经把商品模块的接口调试成功，现在开始修改订单模块

--- takeout_01/product/urls.py
+++ takeout_01/product/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('delete_product',views.delete_product),
     path('add_product',views.add_product),
     path('get_sure_product',views.get_sure_product),
+    path('recover_product',views.recover_product),
 ]
 # p1 = Product(product_name="荔枝",price=32.23,describation="超级甜",saler_id=1,sort="水果")
