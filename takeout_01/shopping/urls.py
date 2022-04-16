3b5f4b262062c51612aa8668109c26fcdbd8ba2d
root <root@localhost.localdomain>
2022-04-15 21:59:52 -0400

    这次完成的是地址信息，用户信息，订单模块，地址查询，订单是否到达，广告推荐的模块编写

--- takeout_01/shopping/urls.py
+++ takeout_01/shopping/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
+from django.views import View
 from . import views
-urlpatterns = []
+urlpatterns = [
+    path('add_useraddress_info',views.add_useraddress_info),
+    path('all_useraddress',views.all_useraddress),
+    path('update_useraddress',views.update_useraddress),
+    path('delete_useraddress',views.delete_useraddress),
+    path('update_book_info',views.update_book_info),
+    path('delete_book_info',views.delete_book_info),
+    path('update_info_deliver',views.update_info_deliver),
+    path('delete_info_deliver',views.delete_info_deliver),
+    path('update_get_address',views.update_get_address),
+    path('delete_get_address',views.delete_get_address),
+]
