d840cdaa6177e7593db5d5ec155aca2a42a02769
root <root@localhost.localdomain>
2022-04-22 08:33:59 -0400

    测试失败案例

--- takeout_01/user/tests.py
+++ takeout_01/user/tests.py
@@ -1,3 +1,10 @@
+import email
+from user.models import UserInfo 
 from django.test import TestCase
-
 # Create your tests here.
+class Nytest(TestCase):
+    
+    def test_case(self):
+        UserInfo.objects.create(user_name="孙笑川",email="121@qq.com",pwd="123123",phone="1786733515",nickname="我是你哥哥")
+
+        
