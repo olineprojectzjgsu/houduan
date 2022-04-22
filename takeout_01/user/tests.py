466ccd0f9056bfcd223b4f17bc9f89c0133063d8
root <root@localhost.localdomain>
2022-04-22 08:41:56 -0400

    插入及查询数据

--- takeout_01/user/tests.py
+++ takeout_01/user/tests.py
@@ -6,5 +6,8 @@ class Nytest(TestCase):
     
     def test_case(self):
         UserInfo.objects.create(user_name="sunxiaochuan",email="1210797561@qq.com",pwd="123123",phone="1786733515",nickname="haohaho")
-
+        p = UserInfo.objects.get(user_name="sunxiaochuan")
+        print(p.user_name)
+        print(p.email)
+        print(p.pwd)
         
