913d2d7c6b584082bbff0d952d7af5df2d6baa0e
root <root@localhost.localdomain>
2022-05-11 10:38:02 -0400

    提交shopping部分的测试代码

--- takeout_01/shopping/tests.py
+++ takeout_01/shopping/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.test import TestCase
+from shopping.models import  UserAddressInfo, Book_Info
 # Create your tests here.
+class Mytest(TestCase):
+    def test_case(self):
+        UserAddressInfo.objects.create(province="zhejiang",city="jinhua",address="yongkang")
+        p = UserAddressInfo.objects.get(address="yongkang")
+        print(p.province)
+        print(p.city)
+        print(p.date)
+        Book_Info.objects.create(remark="very bad",sale="10.32",address="yongkang")
+        p = Book_Info.objects.get(id="1")
+        print(p.remark)
+        print(p.sale)
+        print(p.address)
