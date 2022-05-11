14622c337ac565050c49a17e9c23b737ed1c2bc1
root <root@localhost.localdomain>
2022-05-11 10:58:47 -0400

    继续提交shoppingg部分的测试代码

--- takeout_01/shopping/tests.py
+++ takeout_01/shopping/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.test import TestCase
-from shopping.models import  UserAddressInfo, Book_Info
+from shopping.models import  GetAddress, Info_deliver, Shopping_cart, UserAddressInfo, Book_Info
 # Create your tests here.
 class Mytest(TestCase):
+
     def test_case(self):
         UserAddressInfo.objects.create(province="zhejiang",city="jinhua",address="yongkang")
         p = UserAddressInfo.objects.get(address="yongkang")
@@ -10,7 +11,17 @@ class Mytest(TestCase):
         print(p.city)
         print(p.date)
         Book_Info.objects.create(remark="very bad",sale="10.32",address="yongkang")
-        p = Book_Info.objects.get(id="1")
-        print(p.remark)
-        print(p.sale)
-        print(p.address)
+        p1 = Book_Info.objects.get(id="1")
+        print(p1.remark)
+        print(p1.sale)
+        print(p1.address)
+        Shopping_cart.objects.create(product_name="huangmenjia",delivery_info=True)  
+        p2 = Shopping_cart.objects.get(product_name="huangmenjia")
+        print(p2.delivery_info)
+        Info_deliver.objects.create(advertise="huangmenjia")  
+        p3 = Info_deliver.objects.get(advertise="huangmenjia")
+        print(p3.advertise)
+        GetAddress.objects.create(address="jinhua",phone_number="12345678901",name="jiliu")
+        p4 = GetAddress.objects.get(name="jiliu")
+        print(p4.address)
+        print(p4.phone_number)
